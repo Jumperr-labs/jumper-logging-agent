0c8c86d4615e69234f82d49b6eeebc2887af8794
seryoni <seryoni@gmail.com>
2017-04-25 09:14:17 +0300

    adding instaler files and removing unneeded part from setup.py

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Install pip and virtualenv
+apt-get install python-pip
+pip install virtualenv
+
+# Create a virtual environment and install the app
+virtualenv venv
+source venv/bin/activate
+python setup.py install
+deactivate
+
+# Setup the jumper agent service
+cp jumper-agent.template jumper-agent.service
+echo "ExecStart=$PWD/venv/bin/python2.7 $PWD/agent_main.py" >> jumper-agent.service
+cp jumper-agent.service /lib/systemd/jumper-agent.service
+ln -s /lib/systemd/jumper-agent.service /etc/systemd/system/jumper-agent.service
+
+# Start the jumper agent service
+systemctl daemon-reload
+systemctl start jumper-agent.service
