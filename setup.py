96db38362b29f637ce83daeca0698cba0f0acdc5
Dan Shemesh <dan@jumper.io>
2017-05-25 18:20:03 +0300

    added support for python2.6

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['future', 'keen'],
+    install_requires=['future', 'keen', 'requests[security]<2.11.0>=2.5'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
